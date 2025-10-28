b218749938693e8bd630d192b51ce4068afc83a9
정충효 <alzkdpf000@gmail.com>
2025-10-28 19:59:10 +0900

    diary detail add

--- src/main/java/com/example/crewstation/service/diary/DiaryServiceImpl.java
+++ src/main/java/com/example/crewstation/service/diary/DiaryServiceImpl.java
@@ -432,6 +432,7 @@ public class DiaryServiceImpl implements DiaryService {
                             fileDAO.saveFile(fileDTO);
                             image.setPostId(request.getPostId());
                             log.info("fileDTO{}::::::::::",fileDTO.toString());
+                            sectionDAO.update(toPostSectionVO(image));
                             sectionFileDTO.setFileId(fileDTO.getId());
                             sectionFileDTO.setImageType(type);
                             sectionFileDTO.setPostSectionId(image.getPostSectionId());
@@ -448,18 +449,19 @@ public class DiaryServiceImpl implements DiaryService {
                         }
                     } else {
                         sectionDAO.update(toPostSectionVO(image));
+                        if (image.getTags() != null) {
+//                log.info("태그가 없어");
+                            image.getTags().forEach((tag) -> {
+                                log.info("태그 추가되빈다.");
+                                tag.setPostSectionFileId(image.getFileId());
+                                postFileTagDAO.save(toPostFileTagVO(tag));
+                            });
+                        }
                     }
                     log.info(":::::::::::::{}", image);
                     log.info(":::::::::::::{}", toPostSectionVO(image).toString());
 
-                    if (image.getTags() != null) {
-//                log.info("태그가 없어");
-                        image.getTags().forEach((tag) -> {
-                            log.info("태그 추가되빈다.");
-                            tag.setPostSectionFileId(image.getFileId());
-                            postFileTagDAO.save(toPostFileTagVO(tag));
-                        });
-                    }
+
                 });
         if (request.getThumbnail() > 0 && request.getNewThumbnail() > 0) {
             log.info("기존 :::{}", request.getThumbnail());
